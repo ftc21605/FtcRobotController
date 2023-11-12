3873638fa8e3532f4e45c372da40448a567dda79
FTC-21605 <ftc21605@gmail.com>
2023-11-12 08:12:01 -0500

    add opencv tests

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/FindRedBlue.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.processors.RedBlueProcessor;
+import org.firstinspires.ftc.vision.VisionPortal;
+
+@Autonomous(name="RedBlue")
+public class FindRedBlue extends OpMode {
+    private RedBlueProcessor visionProcessor;
+    private VisionPortal visionPortal;
+
+    @Override
+    public void init() {
+        visionProcessor = new RedBlueProcessor();
+        visionPortal = VisionPortal.easyCreateWithDefaults(hardwareMap.get(WebcamName.class, "Webcam 1"), visionProcessor);
+    }
+
+    @Override
+    public void init_loop() {
+        telemetry.addData("Yaw", "Press Y (triangle) on Gamepad to reset\n");
+    }
+
+    @Override
+    public void start() {
+        visionPortal.stopStreaming();
+    }
+
+    @Override
+    public void loop() {
+    }
+}
