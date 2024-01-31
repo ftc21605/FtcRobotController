a84eb4657d26c0ed83f80bd68e233552846c11e7
FTC-21605 <ftc21605@gmail.com>
2024-01-31 14:57:37 -0500

    functional left/right turn without yaw reset

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/IMULeftRightTurnTest.java
@@ -0,0 +1,185 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.test;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
+
+@TeleOp(name="Test: IMU Left Right Turn Test", group="ZTest")
+//@Disabled
+public class IMULeftRightTurnTest extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftFrontDrive = null;
+    private DcMotorSimple leftBackDrive = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor rightBackDrive = null;
+    // The IMU sensor object
+    IMU imu;
+    static final double TURN_SPEED = 0.2;
+    double drive_angle = 0;
+    @Override
+    public void runOpMode() {
+        double turn_speed_local = TURN_SPEED;
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        leftFrontDrive  = hardwareMap.get(DcMotor.class, "frontleft");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "frontright");
+        leftBackDrive  = hardwareMap.get(DcMotorSimple.class, "backleft");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "backright");
+
+        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
+        // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
+        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
+        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        
+        // This sample expects the IMU to be in a REV Hub and named "imu".
+        imu = hardwareMap.get(IMU.class, "imu");
+        RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.UP;
+        RevHubOrientationOnRobot.UsbFacingDirection  usbDirection  = RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
+
+        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
+
+        // Now initialize the IMU with this mounting orientation
+        // Note: if you choose two conflicting directions, this initialization will cause a code exception.
+        imu.initialize(new IMU.Parameters(orientationOnRobot));
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        imu.resetYaw();
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+if (gamepad1.b){
+    drive_angle = left_turn(45);
+}
+            if (gamepad1.a){
+                drive_angle = right_turn(45);
+            }
+
+            if (gamepad1.x){
+                imu.resetYaw();
+            }
+            telemetry.addData(">","Press A for 45deg right turn");
+            telemetry.addData(">","Press B for 45deg left turn");
+
+            telemetry.addData(">","current angle %.1f, drive to angle: %.1f", imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES), drive_angle);
+
+            telemetry.update();
+        }
+    }
+    double left_turn(double ANGLE){
+        double current_angle = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
+        double driveto_angle = current_angle + ANGLE;
+        telemetry.addData(">","input: %.1f, current angle %.1f, driveto: %.1f", ANGLE, current_angle, driveto_angle );
+        telemetry.update();
+
+        leftFrontDrive.setPower(-TURN_SPEED);
+        rightFrontDrive.setPower(TURN_SPEED);
+        leftBackDrive.setPower(-TURN_SPEED);
+        rightBackDrive.setPower(TURN_SPEED);
+        if (driveto_angle > 180)
+        {
+            while ((current_angle = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES))> 0 || current_angle > (driveto_angle - 360)) {
+                telemetry.addData(">","angle %.1f", current_angle);
+                telemetry.update();
+                sleep(1);
+            }
+
+        }
+        else {
+            while ((current_angle = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES)) < driveto_angle) {
+//            orientation = imu.getRobotYawPitchRollAngles();
+                telemetry.addData(">", "angle %.1f", current_angle);
+                telemetry.update();
+                sleep(1);
+            }
+        }
+        leftFrontDrive.setPower(0);
+        rightFrontDrive.setPower(0);
+        leftBackDrive.setPower(0);
+        rightBackDrive.setPower(0);
+        telemetry.addData(">","final angle %.1f", imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES));
+        telemetry.update();
+        return driveto_angle;
+    }
+    double right_turn ( double ANGLE){
+        double current_angle = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
+        double driveto_angle = current_angle - ANGLE;
+        telemetry.addData("right turn: ","input: %.1f, current angle %.1f, driveto: %.1f", ANGLE, current_angle, driveto_angle );
+        telemetry.update();
+         leftFrontDrive.setPower(TURN_SPEED);
+        rightFrontDrive.setPower(-TURN_SPEED);
+        leftBackDrive.setPower(TURN_SPEED);
+        rightBackDrive.setPower(-TURN_SPEED);
+        if (driveto_angle < -180)
+        {
+            //current_angle = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
+            while ((current_angle = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES)) < 0 || current_angle > (driveto_angle + 360)) {
+                telemetry.addData(">","angle %.1f", current_angle);
+                telemetry.update();
+                sleep(1);
+            }
+        }
+        else {
+            while ((current_angle = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES)) > driveto_angle) {
+                telemetry.addData(">", "angle %.1f", current_angle);
+                telemetry.update();
+                sleep(1);
+            }
+        }
+        leftFrontDrive.setPower(0);
+        rightFrontDrive.setPower(0);
+        leftBackDrive.setPower(0);
+        rightBackDrive.setPower(0);
+        return driveto_angle;
+    }
+
+}
