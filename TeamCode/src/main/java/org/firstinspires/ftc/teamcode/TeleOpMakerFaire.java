bcaa7f08f1013602f18b1d5a97e749e807663e66
FTC-21605 <ftc21605@gmail.com>
2024-06-07 22:16:57 -0400

    gradle update

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMakerFaire.java
@@ -0,0 +1,222 @@
+/* Copyright (c) 2021 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "TeleOp MakerFaire", group = "AWallace")
+//@Disabled
+public class TeleOpMakerFaire extends OurLinearOpBase {
+
+     boolean pad1_x_pushed = false;
+     boolean pad1_a_pushed = false;
+
+    boolean pad1_y_pushed = false;
+    boolean pad1_b_pushed = false;
+
+    boolean pad2_leftbumper_pressed = false;
+    boolean pad2_rightbumper_pressed = false;
+    boolean pad2_dpad_pressed = false;
+
+    @Override
+    public void runOpMode() {
+	setup_drive_motors();
+	setup_planeservo();
+	setup_elevator();
+	setup_intake();
+	setup_hanger();
+	setup_pixel_lift();
+	setup_pixel_bucket();
+	setup_pixel_transport();
+ 	setup_bucketback();
+	setup_bucketfront();
+        bucketback_release();
+
+	// Wait for the game to start (driver presses PLAY)
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+	    PixelLift.setPower(0);
+	    Hanger.setPower(0);
+	    PixelTransport.setPower(0);
+            double max;
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+	    telemetry.addData(">", "left joystick up/down forward/backward");
+	    telemetry.addData(">", "left joystick left/right left/right");
+	    telemetry.addData(">", "right joystick left/right turn left/right");
+	    telemetry.addData(">", "left trigger intake in");
+	    telemetry.addData(">", "right trigger intake out");
+	    //            telemetry.addData(">", "left bumper + right stick: hang");
+            telemetry.addData(">", "Press X for plane launch");
+            telemetry.addData(">", "Press X again for load plane");
+	    //            telemetry.addData(">", "Press Y for hook deploy");
+	    //            telemetry.addData(">", "Press B for hook retract");
+	    //            telemetry.addData(">", "Press A for emergency hook stop");
+	    //            telemetry.addData(">", "Press DPad Up for hanging");
+            telemetry.addData(">", "Press DPad Left for moving sideways to the left");
+            telemetry.addData(">", "Press DPad Right for moving sideways to the right");
+	    //            telemetry.addData(">", "Press Right Bumper for Intake continuous in");
+	    //            telemetry.addData(">", "Press Left Bumper for Intake continuous out");
+
+	    //            telemetry.addData(">", "Pad2: Press A tilt bucket forward");
+	    //            telemetry.addData(">", "Pad2: Press B tile bucket backward");
+	    //            telemetry.addData(">", "Pad2: Press left bumper toggle back lock");
+	    //            telemetry.addData(">", "Pad2: Press right bumper toggle front lock");
+
+            // HERE IS GAMEPAD 2
+            if (gamepad2.x){
+		leftFrontDrive.setPower(0);
+                rightFrontDrive.setPower(0);
+                leftBackDrive.setPower(0);
+                rightBackDrive.setPower(0);
+		Intake.setPower(0);
+		continue;
+	    }
+            // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
+            double axial = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
+            if (Math.abs(axial) < 0.15) {
+                axial = 0.;
+            }
+            axial = axial * 0.7;  // Note: pushing stick forward gives negative value
+            double lateral = 0.;
+            if (Math.abs(gamepad1.left_stick_x) > 0.8) {
+                if (Math.abs(gamepad1.left_stick_y) < 0.3) {
+                    lateral = gamepad1.left_stick_x;
+                }
+            }
+            double yaw = 0;
+	    if (Math.abs(gamepad1.right_stick_x) > 0.15)
+	    {
+		yaw = gamepad1.right_stick_x * 0.6;
+	    }
+            // Combine the joystick requests for each axis-motion to determine each wheel's power.
+            // Set up a variable for each drive wheel to save the power level for telemetry.
+            double leftFrontPower = axial + lateral + yaw;
+            double rightFrontPower = axial - lateral - yaw;
+            double leftBackPower = axial - lateral + yaw;
+            double rightBackPower = axial + lateral - yaw;
+            // Normalize the values so no wheel power exceeds 100%
+            // This ensures that the robot maintains the desired motion.
+            max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+            max = Math.max(max, Math.abs(leftBackPower));
+            max = Math.max(max, Math.abs(rightBackPower));
+
+            if (max > 1.0) {
+                leftFrontPower /= max;
+                rightFrontPower /= max;
+                leftBackPower /= max;
+                rightBackPower /= max;
+            }
+
+            if (gamepad1.dpad_right) {
+                leftFrontPower = 0.3;
+                rightFrontPower = -0.3;
+                leftBackPower = -0.3;
+                rightBackPower = 0.3;
+            }
+            if (gamepad1.dpad_left) {
+                leftFrontPower = -0.3;
+                rightFrontPower = 0.3;
+                leftBackPower = 0.3;
+                rightBackPower = -0.3;
+            }
+
+            // Send calculated power to wheels
+            //    leftFrontDrive.setPower(leftFrontPower);
+            //    rightFrontDrive.setPower(rightFrontPower);
+            //    leftBackDrive.setPower(leftBackPower);
+            //    rightBackDrive.setPower(rightBackPower);
+
+            if (gamepad1.x && !pad1_x_pushed) {
+                pad1_x_pushed = true;
+                launch_plane();
+            }
+            if (!gamepad1.x) {
+                pad1_x_pushed = false;
+            }
+
+
+            double intake_power = 0;
+            if (gamepad1.right_trigger > 0 && gamepad1.left_trigger == 0) {
+                intake_power = gamepad1.right_trigger;
+            }
+            if (gamepad1.left_trigger > 0 && gamepad1.right_trigger == 0) {
+                intake_power = -gamepad1.left_trigger;
+            }
+            Intake.setPower(intake_power);
+
+            //grabber.setPosition(grabber_position);
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
+            telemetry.addData(">", "Pad2: left bumper toggles bucket back lock servo");
+            telemetry.addData(">", "Pad2: right bumper toggles bucket front lock servo");
+	    leftFrontDrive.setPower(leftFrontPower);
+	    rightFrontDrive.setPower(rightFrontPower);
+	    leftBackDrive.setPower(leftBackPower);
+	    rightBackDrive.setPower(rightBackPower);
+        }
+    }
+    public void pixel_release() {
+        double Power = 0.2;
+        int tics = 8;
+        Intake.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Intake.setPower(Power);
+        while (Intake.getCurrentPosition() < tics) {
+            sleep(1);
+        }
+        Intake.setPower(0);
+
+
+    }
+    public void pixel_lock() {
+        double Power = -0.2;
+        int tics = 8;
+        Intake.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        Intake.setPower(Power);
+        while (Intake.getCurrentPosition() < tics) {
+            sleep(1);
+        }
+        Intake.setPower(0);
+
+
+    }
+
+}
+
+
